55dd759d11c9791e9173c735faadf8c09de0784a
OnkelHenky <Alexander.Henky@googlemail.com>
2018-08-20 18:22:07 +0200

    added

---
+++ src/app/not-found.component.ts
@@ -0,0 +1,22 @@
+import { Component} from '@angular/core';
+
+/**
+ * Generic Not Found  Error Page for testing
+ */
+@Component({
+  selector: 'not-found',
+  template: `
+    <section>
+       <div>
+        This is not the data you are looking for.
+        Catch all unimplemented routs ... for Testing only 
+        <a routerLink="/">Got back home </a>
+       </div>
+    </section>
+  `
+})
+export class NotFoundComponent{
+  constructor(){
+
+  }
+}
