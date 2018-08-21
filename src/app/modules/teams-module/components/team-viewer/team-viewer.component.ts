a1402b1ee7a811af4fb6c824e26abccdbd9e174f
OnkelHenky <Alexander.Henky@googlemail.com>
2018-08-21 10:05:13 +0200

    Using async pipe for observables

--- src/app/modules/teams-module/components/team-viewer/team-viewer.component.ts
+++ src/app/modules/teams-module/components/team-viewer/team-viewer.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {Router, ActivatedRoute, Params } from '@angular/router'
+import {Observable} from 'rxjs';
+import {map } from 'rxjs/operators';
+
+interface Team {
+  name: string;
+  id: number
+
+}
 
 @Component({
   selector: 'app-team-viewer',
   templateUrl: './team-viewer.component.html',
   styleUrls: ['./team-viewer.component.css']
 })
-export class TeamViewerComponent implements OnInit {
+export class TeamViewerComponent implements OnInit, OnDestroy {
 
-  team_id: number;
+  params$: Observable<Team>; //Team-Observable, used in the HTML template with async pipe
 
   constructor(
     private router: Router,
@@ -16,15 +24,21 @@ export class TeamViewerComponent implements OnInit {
   ) { }
 
   /**
-   * TODO: Add type/safty check for URL Parameter
+   * TODO: Add type/safety check for URL Parameter
    */
   ngOnInit() {
-    this.route.params
-      .subscribe((data : any) => {
-        console.log('data', data);
-        this.team_id = data.id;
-      });
-
+   this.params$ = this.route.params
+     .pipe(
+       map((data : any) =>  {
+         return {
+           name: 'Foo Team',
+           id: data.id
+         }
+       })
+     );
   }
 
+  ngOnDestroy(){
+    //this.foo.unsubscribe();
+  }
 }
